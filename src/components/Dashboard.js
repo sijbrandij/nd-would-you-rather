2ae55c2a77ae0a94f5194ea9b8a529b1a91d1eae
Karen Sijbrandij <karen.sijbrandij@gmail.com>
2018-08-28 14:59:10 +0200

    Basic dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component { 
+	render() {
+		return (
+			<div>
+				<h3>Would You Rather?</h3>
+				<ul>
+					{this.props.questionIds.map((id) => (
+						<li key={id}>
+							<div>
+								Question id: {id}
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		)
+	}
+}
+
+function mapStateToProps ({ questions }) {
+	return {
+		questionIds: Object.keys(questions)
+			.sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+	}
+}
+
+export default connect(mapStateToProps)(Dashboard)
