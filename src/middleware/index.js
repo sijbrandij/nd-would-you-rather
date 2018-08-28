f5b294eb203bc9b1c39f9cec2b60590522cd9a28
Karen Sijbrandij <karen.sijbrandij@gmail.com>
2018-08-28 14:37:50 +0200

    Fix middleware, hook up data

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,7 @@ import thunk from 'redux-thunk'
 import logger from './logger'
 import { applyMiddleware } from 'redux'
 
-export default applyMiddleware({
+export default applyMiddleware(
 	thunk,
 	logger,
-})
+)
